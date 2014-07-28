ed921051e0e470813b284000b3f49550fc67c8e7
Andrew Patton <andrew@acusti.ca>
2014-07-28 12:21:08 -0400

    Very simple compass config for easy support

---
+++ config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+sass_dir = "./"
+css_dir = "./"
+relative_assets = true
+
+# Development or production?
+environment = :development
+
+# Compress the CSS when in production
+output_style = environment == :production ? :compressed : :nested
+
+# Disable debugging comments that display the original location of your selectors
+line_comments = false
